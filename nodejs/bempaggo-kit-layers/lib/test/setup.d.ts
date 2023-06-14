8d61aa828511789dc4aa80a3c2dd23abf3e628ab
Douglas Hiura Longo <douglas@bempaggo.com.br>
2023-06-14 01:03:11 -0300

    fix: removes ignore folder

--- nodejs/bempaggo-kit-layers/lib/test/setup.d.ts
+++
@@ -1,4 +0,0 @@
-import BemPaggoSdk from "@/app/modules/layers/BemPaggoSDK";
-declare const layers: BemPaggoSdk;
-declare const simulation: (chargeId: number) => Promise<void>;
-export { layers, simulation };
