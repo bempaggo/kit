3c845875b62d66d5097f720acf19db053c8db13b
lucasgomsilva <lucas.silva@bempaggo.com>
2023-06-30 11:47:15 -0300

    fix: changes token

--- nodejs/bempaggo-kit-test/src/test/setup.ts
+++ nodejs/bempaggo-kit-test/src/test/setup.ts
@@ -3,5 +3,5 @@
 import { BempaggoFactory } from "bempaggo-kit/lib/app/modules/Bempaggo";
 import { Environments } from "bempaggo-kit/lib/app/modules/entity/Enum";
 
-export const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidGVuYW50IjoiYmVtcGFnZ29fdXBjcm0iLCJpYXQiOjE2ODgxMjkxNjIsImV4cCI6MTY4ODE4OTE2Mn0.ak1Own6t0fvkghl-BIgIKPDNVVxAUZnUoj0QZzBTRpNC8iBB-XipvSzbUygCYJsJOSzl6l28vbomQGaVkz2oaA"
-export const bempaggoFactory = new BempaggoFactory().create(Environments.SANDBOX, token);
+export const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidGVuYW50IjoiYmVtcGFnZ29fdXBjcm0iLCJpYXQiOjE2ODgxMzUzODAsImV4cCI6MTY4ODE5NTM4MH0.qwFW8HGOm2IfjWj5oLn5pgJNzocX0Q3epKAHKMPkn5uhv3Ah-aezLxqvSF2MzBIjWsmkGiI3MVQRVqIDUav3ow"
+export const bempaggoFactory = new BempaggoFactory().create(Environments.DEVELOPMENT, token);
